91e0079dc3bf1d9a1a7ac3e62d8d82e9fe7ffea2
legowave440 <legowave440@users.noreply.github.com>
2018-01-03 12:26:26 -0500

    seperated functions into seperate file

--- segments.c
+++ segments.c
@@ -2,37 +2,6 @@
  Created by legowave440
  */
 
-#define F_CPU 8000000 //8 MHz
-
-#include <stdio.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <avr/sleep.h>
-#include "segments.h"
-
-#define BRIGHTNESS 100 //This is the typical brightness
-
-int8_t PortB[4]; //These are the segment controls for digits 0-4
-
-//Init
-void initIO(void) {
-    //Initialize the segments for each digit
-
-    DDRD = DDRD | 0b11110000; //Digit output setup
-    DDRB = 0b11111111; //Segment output setup
-}
-
-void sleep(void) {
-    //PORTD = PORTD & 0b00001111; //Turn off the digits
-    //PORTB = 0b00000000; //Turn off the segments
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    cli();
-    sleep_enable();
-    sleep_bod_disable();
-    sei();
-    sleep_cpu();
-}
 
 /*  -   -   -   -   -   Display -   -   -   -   -   -   */
 
@@ -312,62 +281,3 @@ void flashRightDigit(void) {
         updateDigitDisplay(BRIGHTNESS, x);
     }
 }
-
-
-/*  -    -   -   -   -   -   -   -   - Main Program -    -   -   -   -   -   -   -   - */
-
-
-int main(void) {
-	initIO();
-
-
-    //leftScore(0);
-    text("LETS");
-    flashDisplay();
-    text("PLAY");
-    flashDisplay();
-    text("PING");
-    for ( int x = 0; x <= 100; x++) {
-        updateDisplay(255);
-    }
-    for ( int x = 100; x >= 0; x--) {
-        updateDisplay(x);
-    }
-    text("PONG");
-    for ( int x = 0; x <= 100; x++) {
-        updateDisplay(255);
-    }
-    for ( int x = 100; x >= 0; x--) {
-        updateDisplay(255);
-    }
-
-    leftScore(0);
-    rightScore(0);
-
-	while (1) {
-        slowPulse();
-	}
-	return 0; // never reached
-}
-
-
-
-
-
-/* From OLD Library:
-
-
- void Seg::
- void Seg::colon(boolean input) {
- //Colon = input;
- if (Colon == true) {
- PortB[0] = PortB[0]	| B10000000;
- } else {
- PortB[0] = PortB[0] & B01111111;
- }
- }
-
-
- byte Seg::
-
- */
---
+++ segments.h
@@ -0,0 +1,34 @@
+#ifndef SEGMENTS_H
+#define WHATEVER_H_INCLUDED
+/*
+ Created by legowave440
+ */
+
+
+/*  -   -   -   -   -   Display -   -   -   -   -   -   */
+
+//Returns the segment code for a letter or number.
+//This will not work with the letters 'K', 'Q', 'R', 'V', 'W', or 'X'.
+uint8_t chartodigit(char input);
+
+//This replaces the numbers with text.  Only works with ALL CAPS
+void text(char input[4]);
+//This function updates the display
+void updateDisplay(uint8_t brightness);
+//This function updates the display AND sets individual brightnesses for each side
+void updateDigitDisplay(uint8_t brightness_l, uint8_t brightness_r);
+uint8_t inttodig(int input);
+//This sets the score of the 2 righmost digits
+void rightScore(int input);
+//This sets the score of the 2 leftmost digits
+void leftScore(int input);
+
+//VisualEffects ----------------
+void flashDisplay(void);
+
+void slowPulse(void);
+void flashLeftDigit(void);
+
+void flashRightDigit(void);
+
+#endif
